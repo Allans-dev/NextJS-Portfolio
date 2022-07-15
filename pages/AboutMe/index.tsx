d9e0255d67eed2215e21e3e2f18f75ee3e5944ae
Allan <allanswebdev@gmail.com>
2022-07-15 18:24:35 +1000

    added api call to IG + scaffolding

--- pages/AboutMe/index.tsx
+++ pages/AboutMe/index.tsx
@@ -1,8 +1,45 @@
-import type { NextPage } from "next";
+import type { NextPage, GetStaticProps } from "next";
+
 import "/styles/AboutMe.module.css";
 
-const AboutMe: NextPage = () => {
+interface IGcall {
+  [ig: string]: JSON;
+}
+
+const AboutMe: NextPage = ({ ig }: IGcall) => {
+  // console.log(ig);
+
   return <section id="AboutMe">About Me Page</section>;
 };
 
+export const getStaticProps: GetStaticProps = async () => {
+  const igOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": `${process.env.RAPIDAPI_KEY}`,
+      "X-RapidAPI-Host": "instagram28.p.rapidapi.com",
+    },
+  };
+
+  const res = await fetch(
+    "https://instagram28.p.rapidapi.com/medias?user_id=256770551&batch_size=50",
+    igOptions
+  );
+  const ig = await res.json();
+
+  if (!ig) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      ig,
+    },
+  };
+};
+
 export default AboutMe;
+
+//256770551
