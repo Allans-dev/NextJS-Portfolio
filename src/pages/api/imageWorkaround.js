9d68c39757bb8ce23b3d594c328fb75d180336c0
Allan <allanswebdev@gmail.com>
2022-07-29 13:07:24 +1000

    incremental improvements

--- src/pages/api/imageWorkaround.js
+++ src/pages/api/imageWorkaround.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default async (req, res) => {
-  const url = decodeURIComponent(req.query.url);
+  const url = decodeURI(req.query.url);
   const result = await fetch(url);
   const body = await result.body;
   body.pipe(res);
