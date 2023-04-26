846dfdbe5edb4cb938985b08bad2406a1eb65e71
Allan <allanswebdev@gmail.com>
2023-04-26 11:21:12 +1000

    Footer update with socials

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,14 +1,44 @@
-import Image from "next/image";
+import Image from 'next/image';
+import { BsGithub, BsLinkedin, BsInstagram } from 'react-icons/bs';
 
-import styles from "../../styles/Footer.module.css";
+import styles from '../../styles/Footer.module.css';
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <span>code.allan</span>
-      <span className={styles.logo}>
-        <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-      </span>
+      <span>Allan Cheung: Software Guy</span>
+      <ul className={styles.socials}>
+        <li>
+          <a
+            href='https://github.com/Allans-dev'
+            target='_blank'
+            rel='noreferrer'
+            className={styles.nav_link}
+          >
+            <BsGithub size={22} />
+          </a>
+        </li>
+        <li>
+          <a
+            href='https://www.linkedin.com/in/allan-cheung-web/'
+            target='_blank'
+            rel='noreferrer'
+            className={styles.nav_link}
+          >
+            <BsLinkedin size={22} />
+          </a>
+        </li>
+        <li>
+          <a
+            href='https://www.instagram.com/_photosbyallan_/'
+            target='_blank'
+            rel='noreferrer'
+            className={styles.nav_link}
+          >
+            <BsInstagram size={22} />
+          </a>
+        </li>
+      </ul>
     </footer>
   );
 };
