21f9ef381f5d6abb733416b6f9babdcb424c52c3
Allan <allanswebdev@gmail.com>
2023-04-26 11:22:35 +1000

    Header refactor

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,47 +1,44 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image';
+import Link from 'next/link';
 
-import styles from "../../styles/Header.module.css";
+import styles from '../../styles/Header.module.css';
 
 const Header = () => {
   return (
     <section>
-      <div className={styles.navbar}></div>
       <nav className={styles.nav}>
-        <Image
-          src="/vercel.svg"
-          alt="Vercel Logo"
-          width={72}
-          height={16}
-        ></Image>{" "}
-        <Link href="./">
-          <h2 className={styles.headerText}>code.allan</h2>
+        <Link href='./'>
+          <h2 className={styles.headerText}>Allan Cheung: Portfolio</h2>
         </Link>
         <input
           className={styles.menu_toggle}
-          id="menu_toggle"
-          type="checkbox"
+          id='menu_toggle'
+          type='checkbox'
         />
-        <label className={styles.menu_button_container} htmlFor="menu_toggle">
+        <label className={styles.menu_button_container} htmlFor='menu_toggle'>
           <div className={styles.menu_button}></div>
         </label>
         <ul className={styles.menu}>
           <li>
-            <Link href="#portfolio">
-              <a className={styles.nav_link}>Portfolio</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="#aboutMe">
+            <Link href='/about-me'>
               <a className={styles.nav_link}>About Me</a>
             </Link>
           </li>
           <li>
-            <Link href="https://github.com/Allans-dev">
-              <a target="_blank" rel="noreferrer" className={styles.nav_link}>
-                Github
-              </a>
+            <Link href='/portfolio'>
+              <a className={styles.nav_link}>Portfolio</a>
             </Link>
+          </li>
+
+          <li>
+            <a
+              href='https://github.com/Allans-dev'
+              target='_blank'
+              rel='noreferrer'
+              className={styles.nav_link}
+            >
+              Github
+            </a>
           </li>
         </ul>
       </nav>
